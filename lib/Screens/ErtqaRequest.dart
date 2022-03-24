a833859d31e37e09ee0a11fe66713b4b28cfa806
shaaleissa <80845124+shaaleissa@users.noreply.github.com>
2022-03-24 19:44:18 +0300

    More Charites

---
+++ lib/Screens/ErtqaRequest.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EtrqaRequest extends StatelessWidget {
+  const EtrqaRequest({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Screens/JoodRequest.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class JoodRequest extends StatelessWidget {
+  const JoodRequest({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
