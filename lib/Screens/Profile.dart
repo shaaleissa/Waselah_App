c04eb1b884b3788f5fced9c0332bbef201d39fa5
Alaaabdalmohasen <101740847+Alaaabdalmohasen@users.noreply.github.com>
2022-03-24 14:20:43 +0300

    2

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -1,36 +1,40 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 import '../responsive/NavigationBar.dart';
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: Text("WASELA"),
           centerTitle: true,
           leading: IconButton(onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Home1()
-                      ));
-                }, icon: Icon(Icons.arrow_back)),
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Home1()
+                ));
+          }, icon: Icon(Icons.arrow_back)),
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search),),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more)),
           ],
           flexibleSpace: Container(
             decoration: BoxDecoration(
                 gradient: LinearGradient(
-                    colors: [const Color(0xff666a86), Color.fromARGB(255, 185, 131, 137)],
+                    colors: [
+                      const Color(0xff666a86),
+                      Color.fromARGB(255, 185, 131, 137)
+                    ],
                     begin: Alignment.bottomRight,
                     end: Alignment.topLeft)),
           ),
           elevation: 20.0,
           titleSpacing: 20,
-         
+
 
         ),
-        body: Text("Profile"),
+        body: Text("Profile")
+
     );
   }
 }
+
